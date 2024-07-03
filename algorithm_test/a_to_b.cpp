bf3748f9bb7aef9a88435440764e282019039f5a
audrm <audrm@DESKTOP-D25UFNT>
2024-07-03 19:45:55 +0900

    그래프 문제

--- algorithm_test/a_to_b.cpp
+++ algorithm_test/a_to_b.cpp
@@ -1,41 +1,41 @@
-// [백준] 16953번 : A → B [C/C++]
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-int main()
-{
-    string A, B;
-    int count = 1;
-    cin >> A >> B;
-
-    while (1) {
-        if (A == B) {
-            cout << count;
-            break;
-        }
-        else if (stoi(A) > stoi(B)) {
-            cout << -1;
-            break;
-        }
-        else {
-            if (B[B.size() - 1] == '1') {
-                B.pop_back();
-                count++;
-            }
-            else {
-                int temp = stoi(B);
-                if (temp % 2 == 0) {
-                    B = to_string(temp / 2);
-                    count++;
-                }
-                else {
-                    cout << -1;
-                    break;
-                }
-            }
-        }
-    }
-    return 0;
-}
+//// [백준] 16953번 : A → B [C/C++]
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    string A, B;
+//    int count = 1;
+//    cin >> A >> B;
+//
+//    while (1) {
+//        if (A == B) {
+//            cout << count;
+//            break;
+//        }
+//        else if (stoi(A) > stoi(B)) {
+//            cout << -1;
+//            break;
+//        }
+//        else {
+//            if (B[B.size() - 1] == '1') {
+//                B.pop_back();
+//                count++;
+//            }
+//            else {
+//                int temp = stoi(B);
+//                if (temp % 2 == 0) {
+//                    B = to_string(temp / 2);
+//                    count++;
+//                }
+//                else {
+//                    cout << -1;
+//                    break;
+//                }
+//            }
+//        }
+//    }
+//    return 0;
+//}
